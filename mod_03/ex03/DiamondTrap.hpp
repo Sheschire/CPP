e75d209d5c13b4779415c9803ae0c7b12fd0af6e
Sheschire <theo.lemesle42@gmail.com>
2022-03-18 16:30:19 +0100

    commit

---
+++ mod_03/ex03/DiamondTrap.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   DiamondTrap.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/18 16:28:06 by tlemesle          #+#    #+#             */
+/*   Updated: 2022/03/18 16:29:39 by tlemesle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DIAMONDTRAP_HPP
+#define DIAMONTRAP_HPP
+
+#include "ClapTrap.hpp"
+
+class DiamondTrap
+{
+private:
+    std::string _name;
+public:
+    DiamondTrap(/* args */);
+    ~DiamondTrap();
+};
+
+DiamondTrap::DiamondTrap(/* args */)
+{
+}
+
+DiamondTrap::~DiamondTrap()
+{
+}
+
+
+#endif
