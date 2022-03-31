68d968784f7a9d05368abb47d36ed37aa877ae9a
Sheschire <theo.lemesle42@gmail.com>
2022-03-31 16:18:12 +0200

    mkdir includes

--- mod_04/ex03/main.cpp
+++ mod_04/ex03/main.cpp
@@ -6,11 +6,11 @@
 /*   By: tlemesle <tlemesle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 15:49:39 by tlemesle          #+#    #+#             */
-/*   Updated: 2022/03/31 16:01:31 by tlemesle         ###   ########.fr       */
+/*   Updated: 2022/03/31 16:17:17 by tlemesle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes.hpp"
+#include "./includes/includes.hpp"
 
 int main()
 {
